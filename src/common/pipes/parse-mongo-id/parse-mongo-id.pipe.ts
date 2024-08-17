3c3a4df07a93eccc92c45a21248cd3650d69c4ce
Jose Lopez <jflo1937@gmail.com>
2024-08-17 14:36:10 -0400

    CRUD POKEMON con mongoDB.

---
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
@@ -0,0 +1,15 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    // console.log({ value, metadata });
+
+    if(!isValidObjectId(value)) {
+      throw new BadRequestException(`${value} No es un id de mongo válido`);
+    }
+
+    return value;
+  }
+}
